54839c38906e29f7572408e5af25bd8473a47535
Boyce Poleon Jr <development@boyce.pro>
2023-10-14 17:26:17 -0400

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+// Sets up server and connects to the routes
+
+const express = require('express');
+
+const rabbitRoutes = require('./rabbits/routes/rabbits.routes')
+
+const app = express()
+const port = 7070
+
+/* Routes */
+app.use('/rabbits', rabbitRoutes)
+
+app.listen(port, () => console.log(`⚡️[server]: Server is running at http://localhost:${port}`))
